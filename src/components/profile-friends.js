0c2a874e1e4739a62207cf2ae94c91579d16cbb5
farrellm1229 <matt.farrell1@outlook.com>
2022-09-22 23:09:48 -0400

    2-333 Complete - Sorting Friends List
    Top friends are shown first, then the remainder of friends are sorted alphabetically based on last name.

--- src/components/profile-friends.js
+++ src/components/profile-friends.js
@@ -1,6 +1,45 @@
 import { removeChildNodes } from "../utils";
 
 
+const compareStrings = (a, b) => {
+  if (a < b) return -1;
+  if (a > b) return 1;
+
+  return 0;
+}
+
+/**
+ * Function which sorts names by last name 
+ *
+ * @param {object} a first friend
+ * @param {object} b second friend
+ * @return {int} integer denoting which of the friend last name is alphabetically bigger
+ */
+const compareNameOfPerson = (a, b) => {
+  //split on space between first and last name
+  const firstNameA = a.name.split(" ");
+  const firstNameB = b.name.split(" ");
+  //get the value (last name) which is located at last index
+  const lastNameA = firstNameA[firstNameA.length - 1];
+  const lastNameB = firstNameB[firstNameB.length - 1];
+
+  //if both last names are same, refer to the first name
+  return lastNameA === lastNameB ?
+    compareStrings(firstNameA[0], firstNameB[0]) :
+    compareStrings(lastNameA, lastNameB);
+}
+
+const sortFriendsList= (friendsList) => {
+  //top friends 
+  const topFriends = friendsList.filter((friend) => friend?.topFriend);
+  //friends which are not top friends
+  const notTopFriends = friendsList.filter((friend) => !friend?.topFriend);
+  //sort by last name using compareNameOfPerson
+  const sortedRegularFriends = (notTopFriends || []).sort(compareNameOfPerson);
+  //combining them after sorting
+  return [...topFriends, ...sortedRegularFriends];
+}
+
 /**
  * Function which generates a single list-item node based on a dataset
  *
@@ -53,9 +92,11 @@ export const generateFriendsListFromTemplate = (resultsData) => {
 
   if (resultsData.friends && resultsData.friends.length > 0) {
     removeChildNodes(friendsListSection);
+    const friends = sortFriendsList(resultsData.friends);
+
 
     for (let i = 0; i < resultsData.friends.length; i++) {
-      const friendsNode = generateListItemNode(resultsData.friends[i]);
+      const friendsNode = generateListItemNode(friends[i]);
       friendsListSection.appendChild(friendsNode);
     }
   }
