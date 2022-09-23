efed6d0c4020908695df291fa060a7db23f47188
farrellm1229 <matt.farrell1@outlook.com>
2022-09-22 23:35:31 -0400

    3-333 Complete - Button To Collapse Pinned Post
    Added button to collapse/expand pinned posts.

--- src/components/profile-posts.js
+++ src/components/profile-posts.js
@@ -68,5 +68,22 @@ export const generatePinnedPostsFromTemplate = (resultsData) => {
   if (resultsData.pinnedPost) {
     const postNode = generateCardNode(resultsData.pinnedPost);
     pinnedPostsList.appendChild(postNode);
+    //Collapsing pinned post content
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } 
+        else {
+          content.style.display = "block";
+        }
+      });
+    }
   }
 };
